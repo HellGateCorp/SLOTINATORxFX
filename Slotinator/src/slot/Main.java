3e73d43a581b748ccf3c29c759a01a9c3c0cd8a0
Benjamin Wagner <83019866+HellGateCorp@users.noreply.github.com>
2022-06-05 00:11:40 +0200

    Add files via upload

---
+++ Slotinator/src/slot/Main.java
@@ -0,0 +1,8 @@
+package slot;
+
+public class Main extends Slotinator{
+	public static void main(String[] args) {
+		//der Start
+		launch(args);
+	}
+}
---
+++ Slotinator/src/slot/Slotinator.java
@@ -0,0 +1,35 @@
+package slot;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Slotinator extends Application{
+	@Override
+	public void start(Stage meineStage) throws Exception {
+		//eine Instanz von FXMLLoader erzeugen
+		FXMLLoader meinLoader = new FXMLLoader(getClass().getResource("SlotSplit.fxml"));
+		//die Datei laden
+		Parent root = meinLoader.load();
+		//den Controller beschaffen
+		FXMLController meinController = meinLoader.getController();
+		//und die Bühne übergeben
+		meinController.setMeineStage(meineStage);
+		//die Szene erzeugen
+		//an den Konstruktor werden der oberste Knoten und die Größe übergeben
+		Scene meineScene = new Scene(root, 1000, 620);
+		
+		//den Titel über stage setzen
+		meineStage.setTitle("SLOTINATOR");
+		//die Szene setzen
+		meineStage.setScene(meineScene);
+		//und anzeigen
+		meineStage.show();
+		meineStage.setResizable(true);
+		meinController.start();
+	}
+}
+
